243d773cc3c21a2380a77d9ce25134fb9bbe4294
Joseph Tabalon <joseph.tabalon.jr@gmail.com>
2022-02-22 10:13:45 -0800

    complete creation of initial env - begin imports

--- env_info.md
+++ env_info.md
@@ -4,3 +4,7 @@ Environment name: tf_airway
 
 - Python: 3.9
 - Tensorflow: 2.4.1 
+- nibabel: 3.2.2
+- tqdm: 4.62.3
+
+
--- fit_one_image.py
+++ fit_one_image.py
@@ -1,4 +1,17 @@
-# TODO create environment
+# environment name: tf_airway
+
+import numpy as np
+import matplotlib.pyplot as plt
+import nibabel as nib
+
+""" DL imports """
+
+import tensorflow as tf
+from tensorflow.keras import layers
+from tensorflow.keras.models import Model
+#from tensorflow.keras 
+
+
 
 
 
