1e272446d623e2c2855461c2abb9546be2164dec
Joseph Tabalon <joseph.tabalon.jr@gmail.com>
2022-02-24 05:22:01 -0800

    add test_data dir + gitignore

--- fit_one_image.py
+++ fit_one_image.py
@@ -12,10 +12,33 @@ from tensorflow.keras.models import Model
 #from tensorflow.keras 
 
 
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import nibabel as nib
+
+""" DL imports """
+
+import tensorflow as tf
+from tensorflow.keras import layers
+from tensorflow.keras.models import Model
+#from tensorflow.keras 
+
+# TODO read in image and mask
+
+ct_path = "/home/jtabalon/airway_seg/test_data/ct_patients10004O.nii"
+aw_path = "/home/jtabalon/airway_seg/test_data/aw_10004O.nii"
+
+img = nib.load(ct_path).get_fdata()
+mask = nib.load(aw_path).get_fdata()
+
+
+# Visualize CT
+
+plt.imshow(img[:, :, 100])
+plt.show()
 
 
 
 
-
-
-
+# %%
