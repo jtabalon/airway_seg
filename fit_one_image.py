a472314e998b93768e62014b04731e0152e22a83
Joseph Tabalon <joseph.tabalon.jr@gmail.com>
2022-02-24 05:27:30 -0800

    remove extra imports

--- fit_one_image.py
+++ fit_one_image.py
@@ -1,17 +1,5 @@
 # environment name: tf_airway
 
-import numpy as np
-import matplotlib.pyplot as plt
-import nibabel as nib
-
-""" DL imports """
-
-import tensorflow as tf
-from tensorflow.keras import layers
-from tensorflow.keras.models import Model
-#from tensorflow.keras 
-
-
 import os
 import numpy as np
 import matplotlib.pyplot as plt
@@ -32,11 +20,11 @@ aw_path = "/home/jtabalon/airway_seg/test_data/aw_10004O.nii"
 img = nib.load(ct_path).get_fdata()
 mask = nib.load(aw_path).get_fdata()
 
-
+print(img.shape)
 # Visualize CT
 
-plt.imshow(img[:, :, 100])
-plt.show()
+#plt.imshow(img[:, :, 100])
+#plt.show()
 
 
 
