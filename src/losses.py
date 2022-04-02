1e1e9468cde7b56bf875b88f597f3700d3c639cf
Joseph Tabalon <joseph.tabalon.jr@gmail.com>
2022-04-02 15:02:34 -0700

    add losses file and add dice loss

---
+++ src/losses.py
@@ -0,0 +1,6 @@
+import tensorflow as tf
+
+def dice_loss(y_true, y_pred):
+    numerator = 2 * tf.reduce_sum(y_true * y_pred)
+    denominator = tf.reduce_sum(y_true + y_pred)
+    return 1 - numerator / denominator
