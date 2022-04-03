4255531b8a123a451a76ff9baf3a603bd7ec61a8
Joseph Tabalon <joseph.tabalon.jr@gmail.com>
2022-04-02 18:49:00 -0700

    add num epochs

--- src/constant.py
+++ src/constant.py
@@ -4,6 +4,8 @@ VALID_IDS_PATH = "/home/jtabalon/airway_seg/valid_ids.txt"
 TRAIN_DIR = "/home/jtabalon/airway_seg_data/train"
 VALID_DIR = "/home/jtabalon/airway_seg_data/valid"
 
+DATA_DIR = "/home/jtabalon/airway_seg_data/"
+
 PARAMS_PATH = "/home/jtabalon/airway_seg/params.json"
 
 MODEL_INPUT_SIZE = (64,64,64,1)
--- src/main.py
+++ src/main.py
@@ -1,7 +1,4 @@
-
-"""
-Library imports
-"""
+# Library imports
 
 import os
 import numpy as np
@@ -20,30 +17,29 @@ from tensorflow.keras.callbacks import ModelCheckpoint, LearningRateScheduler
 from tensorflow.keras import backend as keras
 from tensorflow.keras import Input
 
-"""
-Custom Imports
-"""
+# Custom Imports
 
-from constant import TRAIN_IDS_PATH, VALID_IDS_PATH, TRAIN_DIR, VALID_DIR, MODEL_INPUT_SIZE, PARAMS_PATH
+from constant import TRAIN_IDS_PATH, VALID_IDS_PATH, TRAIN_DIR, VALID_DIR, MODEL_INPUT_SIZE, PARAMS_PATH, DATA_DIR
 from data_loader import data_generator, get_ids
 from losses import dice_loss
 from model_architectures import unet
 
-# TODO: Add file for file paths
-
 def main(args):
     # TODO: hyperparameters
-    if args.use_json_file:
-        with open(use_json_file, "r") as read_file:
-            params = json.load(read_file)
-            patch_size = params["patch_size"]
-            batch_size = params["batch_size"]
-            learning_rate = params["learning_rate"]
-            num_epochs = params["num_epochs"]
-
-
-
-
+    # TODO: using json file
+    # if args.use_json_file:
+    #     with open(args.use_json_file, "r") as read_file:
+    #         params = json.load(read_file)
+    #         patch_size = params["patch_size"]
+    #         batch_size = params["batch_size"]
+    #         learning_rate = params["learning_rate"]
+    #         num_epochs = params["num_epochs"]
+    
+    patch_size = args.patch_size
+    batch_size = args.batch_size
+    learning_rate = args.learning_rate
+    # num_epochs = args.num_epochs
+    print(patch_size, batch_size, learning_rate)
 
     # Read in patient ids
     train_ids = get_ids(TRAIN_IDS_PATH)
@@ -56,17 +52,20 @@ def main(args):
     # TODO: Test dice loss
     model.compile(optimizer=Adam(lr=learning_rate), loss=dice_loss)
     # TODO: Callbacks + model.fit
-    
 
     train_generator = data_generator(ids=train_ids, data_dir=TRAIN_DIR, batch_size=batch_size, patch_size=patch_size)
     valid_generator = data_generator(ids=valid_ids, data_dir=VALID_DIR, batch_size=batch_size, patch_size=patch_size)
 
-
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     # TODO: Configure argument parser
-    parser.add_argument('--use_json_file', type=str, default=PARAMS_PATH)
+    parser.add_argument("-json", '--use_json_file', type=str, default=PARAMS_PATH)
+    parser.add_argument("-p", "--patch_size", type=int, default=64)
+    parser.add_argument("-bs", "--batch_size", type=int, default=1)
+    parser.add_argument("-lr", "--learning_rate", type=int, default=1e-4)
+    parser.add_argument("-d", "--data_dir", type=str, default=DATA_DIR)
+    parser.add_argument("-n", "--num_epochs", type=str, default=1000)
+
     # parser.add_argument('--data_dir', type=str, default=) # TODO: Add default directory
     args = parser.parse_args()
 
