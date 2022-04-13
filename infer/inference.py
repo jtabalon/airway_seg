bc239d6cf7bc8fe1b2e296472cd7f0e1a2eb1185
Joseph Tabalon <joseph.tabalon.jr@gmail.com>
2022-04-13 05:33:49 -0700

    add todos

--- infer/inference.py
+++ infer/inference.py
@@ -1,20 +1,29 @@
 # Library Imports
-import tensorflow as tf
 import argparse
+import numpy as np
+import tensorflow as tf
 
 # Custom Imports
 
 def main():
+    # TODO: Load model + weights
+
+
+    # TODO: Load image
+
+
+    # TODO: Iterate through image given patch size (start with 64)
+
     print(f"hello world")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     # TODO: Configure arguments
 
-    parser.add_argument("-bs", "--batch_size", type=int, default=1)
+    parser.add_argument("-p", "--patch_size", type=int, default=64)
+    # parser.add_argument("-w", "--weights_path", type=str, default=)
+    # parser.add_argument("-i", "--image_dir", type=str, default=)
 
     args = parser.parse_args()
 
     main(args)
-
-
