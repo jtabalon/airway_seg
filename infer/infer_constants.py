ca027ad3d55915435e6c2068bc631a8e32c170ae
Joseph Tabalon <joseph.tabalon.jr@gmail.com>
2022-04-13 17:35:42 -0700

    add test_img dir in infer_constants

---
+++ infer/infer_constants.py
@@ -0,0 +1,15 @@
+TRAIN_IDS_PATH = "/home/jtabalon/airway_seg/train_ids.txt"
+VALID_IDS_PATH = "/home/jtabalon/airway_seg/valid_ids.txt"
+
+TRAIN_DIR = "/home/jtabalon/airway_seg_data/train"
+VALID_DIR = "/home/jtabalon/airway_seg_data/valid"
+
+DATA_DIR = "/home/jtabalon/airway_seg_data/"
+
+PARAMS_PATH = "/home/jtabalon/airway_seg/params.json"
+CKPT_PATH = "/home/jtabalon/airway_seg/run2_bs_2"
+
+# inference
+TEST_IMG = "/home/jtabalon/airway_seg_data/test/ct/ct_patients10006S.nii"
+
+MODEL_INPUT_SIZE = (64,64,64,1)
